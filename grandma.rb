5c88196f57dfbb8bdcd4dca8162d1bc011b6bb78
Louie O'Grady <l.ogrady@gmail.com>
2019-01-14 20:41:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if (phrase == phrase.upcase)
-    puts 
-    elsif
-    puts
+    return "NO, NOT SINCE 1938!"
+    elsif 
+    return
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    return"HUH?! SPEAK UP, SONNY!"
     
